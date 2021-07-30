75d089e1a7f49d7848df1ff1aa8e1292ef046641
Brian Prince <magister.prince@pm.me>
2021-07-30 06:44:27 -0500

    Initial commit, starting code for expense tracker project.

---
+++ main.js
@@ -0,0 +1,9 @@
+var date = document.querySelector('#date');
+var description = document.querySelector('#description');
+var amount = document.querySelector('#amount');
+var table = document.querySelector('#table');
+
+// Add event listener to 'Add Expense' button
+document.querySelector('#add').addEventListener('click', function() {
+  
+});
