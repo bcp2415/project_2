22ba16e2ddd65051624bc380ec11cd03f62ac50b
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-07-31 13:23:04 -0500

    Adds init function to set variables and place focus in first input field.

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-var date = document.querySelector('#date');
-var description = document.querySelector('#description');
-var amount = document.querySelector('#amount');
-var table = document.querySelector('#table');
-date.focus();
+function init() {
+  var date = document.querySelector('#date');
+  var description = document.querySelector('#description');
+  var amount = document.querySelector('#amount');
+  var table = document.querySelector('#table');
+  date.focus();
+};
 
 // Add event listener to 'Add Expense' button
 document.querySelector('#add').addEventListener('click', function() {
