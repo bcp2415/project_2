412a38f8a56cd9e2c7e79a107642fb106911cb93
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-07-31 13:23:03 -0500

    initial commit

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ var date = document.querySelector('#date');
 var description = document.querySelector('#description');
 var amount = document.querySelector('#amount');
 var table = document.querySelector('#table');
+date.focus();
 
 // Add event listener to 'Add Expense' button
 document.querySelector('#add').addEventListener('click', function() {
-  
+  AddnewExpense(date, description, amount);
 });
+
+// function for adding new Expense
+function(date, description, amount) {
+
+};
