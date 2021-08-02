27d57cd25cb43433cd34ff6a03c1ec74a529ae1e
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-08-02 14:13:57 -0500

    Changes line in addNewExpense fcn so date.value is reset to 0, not a blank string --- also results in date.focus line starting to work again.

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
 function init() {
-  var date = document.querySelector('#date');
-  var description = document.querySelector('#description');
-  var amount = document.querySelector('#amount');
-  var table = document.querySelector('#table');
+    var date = document.querySelector('#date');
+    var description = document.querySelector('#description');
+    var amount = document.querySelector('#amount');
+    var table = document.querySelector('#table');
 
-  // Initialize all 3 input fields by clearing them and placing focus in date:
-  date.value = "";
-  description.value = "";
-  amount.value = 0;
-  date.focus();
+    // Initialize all 3 input fields by clearing them and placing focus in date:
+    date.value = "";
+    description.value = "";
+    amount.value = 0;
+    date.focus();
 
-  // Add event listener to 'Add Expense' button
-  document.querySelector('#add').addEventListener('click', function() {
-    addNewExpense(date, description, amount);
-  });
+    // Add event listener to 'Add Expense' button
+    document.querySelector('#add').addEventListener('click', function() {
+        addNewExpense(date, description, amount);
+    });
 
-  // Create headings and basic structure for table:
-  table.insertAdjacentHTML('afterbegin', '<tr><td>Date</td><td>Description</td><td>Amount</td></tr>');
+    // Create headings and basic structure for table:
+    table.insertAdjacentHTML('afterbegin', '<tr><td>Date</td><td>Description</td><td>Amount</td></tr>');
 };
 
 // Add new Expense
 function addNewExpense(date, description, amount) {
 
-  // Read values of 3 inputs:
-  var newDate = date.value;
-  var newDescription = description.value;
-  var newAmount = Number(amount.value);
+    // Read values of 3 inputs:
+    var newDate = date.value;
+    var newDescription = description.value;
+    var newAmount = Number(amount.value);
 
-  // Write to table
-  table.insertAdjacentHTML('beforeend', `<tr>
+    // Write to table
+    table.insertAdjacentHTML('beforeend', `<tr>
     <td>${newDate}</td>
     <td>${newDescription}</td>
     <td>${newAmount}</td>
@@ -46,13 +46,13 @@ function addNewExpense(date, description, amount) {
       <td>${totalExp}</td>
       </tr>`);
 
-  // Clear input fields
-  date.value = "";
-  description.value = "";
-  amount.value = "";
+    // Clear input fields
+    date.value = "";
+    description.value = "";
+    amount.value = 0;
 
-  // Put focus back in date fields
-  date.focus();
+    // Put focus back in date fields
+    date.focus();
 
 };
 
