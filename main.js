88f0768a0d2192687e465654b7747842c6dc73a1
bcp2415 <64213121+bcp2415@users.noreply.github.com>
2021-08-02 15:58:46 -0500

    Added delete button and event listener; so far DeleteRow fcn is a shell.

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var amount = document.querySelector('#amount');
 var table = document.querySelector('#table');
 var wrapper = document.querySelector('#wrapper');
 var totalExp = document.querySelector('#totalExp');
+var buttonCount = 0;
 
 function init() {
 
@@ -35,12 +36,22 @@ function addNewExpense(date, description, amount) {
     var newDescription = description.value;
     var newAmount = Number(amount.value);
 
+    // Increment counter to create unique ID for delete button:
+    buttonCount++;
+
     // Write to table
     table.insertAdjacentHTML('beforeend', `<tr>
     <td>${newDate}</td>
     <td>${newDescription}</td>
     <td>${newAmount}</td>
+    <td><button id = button${buttonCount}
+        >x</button></td>
     </tr>`);
+
+    // Add event listener to delete button
+    document.querySelector(`#button${buttonCount}`).addEventListener('click', function() {
+        DeleteRow(buttonCount);
+    });
 
     // Calculate total expenses
     total = total + newAmount;
@@ -57,6 +68,10 @@ function addNewExpense(date, description, amount) {
     // Put focus back in date fields
     date.focus();
 
+};
+
+function DeleteRow(buttonCount) {
+    console.log('Delete row function called.');
 };
 
 init();
